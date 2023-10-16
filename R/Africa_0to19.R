c7eac4adfdb3a527cc8a7211668e84613959c45d
Sumali Bajaj <34308572+sumalibajaj@users.noreply.github.com>
2023-10-16 14:03:00 +0100

    updated percentages

--- R/Africa_0to19.R
+++ R/Africa_0to19.R
@@ -2,21 +2,26 @@ library(dplyr)
 
 # From here: https://population.un.org/dataportal/data/indicators/46/locations/903/start/2023/end/2050/table/pivotbylocation
 # Some rows are repeated, so make sure to extract distinct rows only
-dat_read <- read.csv("data/africa/unpopulation_dataportal_20231016105945.csv")
+dat_read <- read.csv("data/africa/unpopulation_dataportal_20231016133702.csv")
 
 # Keeping data for children aged 0-19
-dat <- dat_read %>%
+dat_0to19 <- dat_read %>%
   select(Location, Time, Age, Value) %>%
   filter(Age %in% c("0-4", "5-9", "10-14", "15-19")) %>%
   distinct() 
 
-# Children as of 2023
-dat_23 <- dat %>%
-  filter(Time == 2023)
-sum(dat_23$Value)
-
-dat_year <- dat %>%
+dat_0to19 <- dat_0to19 %>%
   group_by(Time) %>%
   summarise(pop = sum(Value))
 
-write.csv(dat_year, file = "data/processed/africa_0to19.csv")
+# Total population by year
+dat_all <- dat_read %>%
+  select(Location, Time, Age, Value) %>%
+  distinct() %>%
+  group_by(Time) %>%
+  summarise(pop_all = sum(Value))
+
+dat_final <- left_join(dat_0to19, dat_all, by = "Time") %>%
+  mutate(percent = (pop/pop_all)*100)
+
+write.csv(dat_final, file = "data/processed/africa_0to19.csv")
